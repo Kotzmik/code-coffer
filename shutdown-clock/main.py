ed0f7c0e35438d37d944e1b93ae6a31b915c0ab4
Kotzmik <47783794+Kotzmik@users.noreply.github.com>
2023-06-10 17:52:45 +0200

    added clean exit

--- shutdown-clock/main.py
+++ shutdown-clock/main.py
@@ -3,11 +3,21 @@ import time
 import os
 import pytermgui as ptg
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('time_left', type=int, nargs='?', default=30)
-    args=parser.parse_args()
+def cycle(manager):
+    time.sleep(1)
+    manager.compositor.stop()
 
+def handle_exit(manager: ptg.WindowManager, window: ptg.Window):
+    manager.remove(window)
+    # for windows
+    if os.name == 'nt':
+        os.system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        os.system('clear')
+    exit()
+
+def main():
     ptg.Splitter.set_char("separator", " ")
     matrix_gray = ptg.PixelMatrix(6, 8, default="gray")
     matrix_green = ptg.PixelMatrix(6, 8, default="green")
@@ -25,10 +35,12 @@ if __name__ == "__main__":
         window = ptg.Window(splitter)
         manager.add(window, assign="body")
         manager.compositor.run()
-        time.sleep(1)
-        manager.compositor.stop()
+        cycle(manager)
         splitter += matrix_green
         manager.compositor.run()
+        cycle(manager)
+
+        handle_exit(manager, window)
         # splitter.pop()
         # splitter += matrix_gray
         # i=0
@@ -45,3 +57,11 @@ if __name__ == "__main__":
         #     switch= not switch
         #     i+=1
         #     if i==10: break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('time_left', type=int, nargs='?', default=30)
+    args=parser.parse_args()
+
+    main()
